740be0a464f4631d9a87da4be02e101fc3a54ffc
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2022-09-06 22:41:23 +0600

    Add files via upload

---
+++ odd pairs.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,p=0,s=0,r=0;
+        cin>>x;
+        if(x%2==0)
+        {
+            s=(x/2);
+            r=(x/2);
+        }
+        else
+        {
+            s=(x/2);
+            r=((x+1)/2);
+        }
+        p=s*r;
+        if(x<=1)
+        {
+            cout<<"0\n";
+        }
+        else
+        {
+            cout<<p*2<<endl;
+        }
+    }
+    return 0;
+}
