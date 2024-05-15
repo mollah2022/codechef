458d3b6027dc6b6391dbdb192284ef3a087cb51d
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-16 00:47:53 +0600

    Add files via upload

--- codechef.cpp
+++ codechef.cpp
@@ -5,35 +5,16 @@ int main() {
 	// your code goes here
 	int t; cin >> t;
 	while(t--){
-	       int n; cin >> n;
-	       vector<int>v1;
-	       for(int i=0;i<n;i++){
-	             int y; cin >> y;
-	             v1.push_back(y);
-	       }
-	       sort(v1.begin(),v1.end());
-	       reverse(v1.begin(),v1.end());
-	       int l = v1.size();
-	       int sum1=0,sum2=0;
-	       if(l%2!=0){
-                       for(int i=0;i<=v1.size()/2;i++){
-	             sum1+=v1[i];
-	       }
-	        for(int i=v1.size()/2+1;i<v1.size();i++){
-	             sum2+=v1[i];
-	       }
-               cout << sum1-sum2 << endl;
-               continue;
-	       }
-	       
-	       for(int i=0;i<v1.size()/2;i++){
-	             sum1+=v1[i];
-	       }
-	        for(int i=v1.size()/2;i<v1.size();i++){
-	             sum2+=v1[i];
-	       }
-
-	       cout << sum1-sum2 << endl;
+	       int n,m; cin >> n >> m;
+	      while(m--){
+	      	    if(n<1000){
+	      	    	  n+=1000;
+	      	    }
+	      	    else{
+	      	    	  n*=2;
+	      	    }
+	      }
+	      cout << n << endl;
 	}
-  return 0;
+return 0;
 }
