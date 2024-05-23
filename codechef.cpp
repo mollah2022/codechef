04d1dae34809308022e48ca303fb947ec925db8e
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-24 00:38:20 +0600

    Add files via upload

--- codechef.cpp
+++ codechef.cpp
@@ -1,20 +1,31 @@
-#include <bits/stdc++.h>
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
 using namespace std;
+#define ll   long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0);
+#define tst  int t; cin >> t; while(t--)
+#define yes  cout << "YES\n";
+#define no   cout << "NO\n";
 
-int main() {
-	// your code goes here
-	int t; cin >> t;
-	while(t--){
-	       int n,m; cin >> n >> m;
-	      while(m--){
-	      	    if(n<1000){
-	      	    	  n+=1000;
-	      	    }
-	      	    else{
-	      	    	  n*=2;
-	      	    }
-	      }
-	      cout << n << endl;
-	}
-return 0;
+int32_t main(){
+           FAST
+             tst{
+                    int n; cin >> n;
+                    string s1; cin >> s1;
+                    int count0 = 0,count1 = 0;
+                    for(int i=0;i<s1.size();i++){
+                           if(s1[i]=='0'){
+                                 if(i==0 || s1[i-1]!='0'){
+                                        count0++;
+                                 }
+                           }
+                           else{
+                                    if(i==0 || s1[i-1]!='1'){
+                                          count1++;
+                                    }
+                           }
+                    } 
+                    cout << min(count0,count1) <<endl;
+             }
+  return 0;
 }
